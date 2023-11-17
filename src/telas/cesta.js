d1c3c7eef584961450454a1208254072c3bdc200
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-11-17 10:52:47 -0300

    .

--- src/telas/cesta.js
+++ src/telas/cesta.js
@@ -8,7 +8,7 @@ const width = Dimensions.get('screen').width;
 export default function Cesta(){
     return <>
     <Image source={topo} style={estilos.topo} />
-    <Text>Detalhes da Cesta</Text>
+    <Text style={estilos.titulo} >Detalhes da Cesta</Text>
     </> 
 }
 
@@ -16,5 +16,16 @@ const estilos = StyleSheet.create({
     topo:{
         width:"100%",
         height: 578/768 * width,
+    },
+
+    titulo:{
+        width:"100%",
+        position:"absolute",
+        textAlign:"center",
+        color:"white",
+        fontSize: 16,
+        lineHeight: 26,
+        fontWeight: "bold",
+        padding: 16,
     }
 })
