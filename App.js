56f621538461456b311f0c50d441ea9b5d18d8ca
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-11-24 13:03:50 -0300

    .

--- App.js
+++ App.js
@@ -1,7 +1,17 @@
 import { StatusBar ,StyleSheet, Text, SafeAreaView} from 'react-native';
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 import Cesta from './src/telas/cesta';
 
 export default function App() {
+  const [fonteCarregada] = useFonts({
+    MontserratRegular: Montserrat_400Regular,
+    MontserratBold: Montserrat_700Bold,
+  });
+
+  if (!fonteCarregada) {
+    return <View/>;
+  }
+
   return (
     <SafeAreaView>
       <StatusBar/>
--- src/telas/cesta.js
+++ src/telas/cesta.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { Image, StyleSheet, Dimensions, Text } from "react-native";
+import { Image, StyleSheet, Dimensions, Text, View } from "react-native";
 
 import topo from '../../assets/topo.png';
+import logoFazenda from '../../assets/logo.png';
 
 const width = Dimensions.get('screen').width;
 
@@ -9,7 +10,16 @@ export default function Cesta(){
     return <>
     <Image source={topo} style={estilos.topo} />
     <Text style={estilos.titulo} >Detalhes da Cesta</Text>
-    <Text style={estilos.subtitulo} >Cesta de verduras</Text>
+
+    <View style={estilos.cesta}>
+        <Text style={estilos.subtitulo}>Cesta de Verduras</Text>
+        <View style={estilos.fazenda}>
+            <Image source={logoFazenda} style={estilos.imagemFazenda}/>
+            <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+        </View>
+        <Text style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente da fazendo direto para a sua cozinha</Text>
+        <Text style={estilos.preco}>40,00</Text>
+    </View>
     </> 
 }
 
@@ -34,10 +44,46 @@ const estilos = StyleSheet.create({
 
     subtitulo:{
         width:"100%",
-        color:"black",
+        color:"#464646",
         fontSize: 36,
-        lineHeight: 36,
+        lineHeight: 42,
+        fontFamily: "MontserratBold",
+    },
+
+    cesta: {
+        paddingHorizontal:16,
+        paddingVertical: 16,
+    },
+
+    fazenda:{
+        flexDirection: "row",
+        paddingVertical: 12,
+        
+    },
+
+    imagemFazenda:{
+        width: 32,
+        height:32,
+    },
+
+    nomeFazenda:{
+        fontSize:16,
+        lineHeight: 26,
+        marginLeft: 12,
+        fontFamily: "MontserratRegular",
+    },
+
+    descricao: {
+        color: '#a3a3a3',
+        fontSize:16,
+        lineHeight: 26,
+    },
+
+    preco:{
+        color:'#2A9F85',
+        fontSize:26,
+        lineHeight: 42,
+        marginTop: 8,
         fontWeight: "bold",
-        padding: 20,
-    }
+    },
 })
