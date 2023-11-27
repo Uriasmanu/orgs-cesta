d02cee4fa5e20a795dfbf5dc2031c3c24b079267
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-11-27 10:43:11 -0300

    .

--- src/telas/cesta.js
+++ src/telas/cesta.js
@@ -1,89 +1,77 @@
-import React from "react";
-import { Image, StyleSheet, Dimensions, Text, View } from "react-native";
+import React from 'react';
+import { StyleSheet, Image, Dimensions, Text, View } from 'react-native';
 
 import topo from '../../assets/topo.png';
-import logoFazenda from '../../assets/logo.png';
+import logo from '../../assets/logo.png';
 
 const width = Dimensions.get('screen').width;
 
-export default function Cesta(){
-    return <>
+export default function Cesta() {
+  return <>
     <Image source={topo} style={estilos.topo} />
-    <Text style={estilos.titulo} >Detalhes da Cesta</Text>
+    <Text style={estilos.titulo}>Detalhe da cesta</Text>
 
     <View style={estilos.cesta}>
-        <Text style={estilos.subtitulo}>Cesta de Verduras</Text>
-        <View style={estilos.fazenda}>
-            <Image source={logoFazenda} style={estilos.imagemFazenda}/>
-            <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
-        </View>
-        <Text style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente da fazendo direto para a sua cozinha</Text>
-        <Text style={estilos.preco}>40,00</Text>
+      <Text style={estilos.nome}>Cesta de Verduras</Text>
+      <View style={estilos.fazenda}>
+        <Image source={logo} style={estilos.imagemFazenda} />
+        <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+      </View>
+      <Text style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha</Text>
+      <Text style={estilos.preco}>R$ 40,00</Text>
     </View>
-    </> 
+  </>
 }
 
-
-
 const estilos = StyleSheet.create({
-    topo:{
-        width:"100%",
-        height: 578/768 * width,
-    },
-
-    titulo:{
-        width:"100%",
-        position:"absolute",
-        textAlign:"center",
-        color:"white",
-        fontSize: 16,
-        lineHeight: 26,
-        fontWeight: "bold",
-        padding: 16,
-    },
-
-    subtitulo:{
-        width:"100%",
-        color:"#464646",
-        fontSize: 36,
-        lineHeight: 42,
-        fontFamily: "MontserratBold",
-    },
-
-    cesta: {
-        paddingHorizontal:16,
-        paddingVertical: 16,
-    },
-
-    fazenda:{
-        flexDirection: "row",
-        paddingVertical: 12,
-        
-    },
-
-    imagemFazenda:{
-        width: 32,
-        height:32,
-    },
-
-    nomeFazenda:{
-        fontSize:16,
-        lineHeight: 26,
-        marginLeft: 12,
-        fontFamily: "MontserratRegular",
-    },
-
-    descricao: {
-        color: '#a3a3a3',
-        fontSize:16,
-        lineHeight: 26,
-    },
-
-    preco:{
-        color:'#2A9F85',
-        fontSize:26,
-        lineHeight: 42,
-        marginTop: 8,
-        fontWeight: "bold",
-    },
-})
+  topo: {
+    width: "100%",
+    height: 578 / 768 * width,
+  },
+  titulo: {
+    width: "100%",
+    position: "absolute",
+    textAlign: "center",
+    fontSize: 16,
+    lineHeight: 26,
+    color: "white",
+    fontWeight: "bold",
+    padding: 16,
+  },
+  cesta: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+  },
+  nome: {
+    color: "#464646",
+    fontSize: 26,
+    lineHeight: 42,
+    fontFamily: "MontserratBold",
+  },
+  fazenda: {
+    flexDirection: "row",
+    paddingVertical: 12,
+  },
+  imagemFazenda: {
+    width: 32,
+    height: 32,
+  },
+  nomeFazenda: {
+    fontSize: 16,
+    lineHeight: 26,
+    marginLeft: 12,
+    fontFamily: "MontserratRegular",
+  },
+  descricao: {
+    color: "#A3A3A3",
+    fontSize: 16,
+    lineHeight: 26,
+  },
+  preco: {
+    color: "#2A9F85",
+    fontWeight: "bold",
+    fontSize: 26,
+    lineHeight: 42,
+    marginTop: 8,
+  },
+});
